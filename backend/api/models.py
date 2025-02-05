40a3be7aa3c89007758b32133c66db13f2a38ece
Zafarbek Yunusov <zyunusov30@gmail.com>
2025-02-05 18:54:19 +0500

    added post and category

--- backend/api/models.py
+++ backend/api/models.py
@@ -60,3 +60,60 @@ def save_user_profile(sender, instance, **kwargs):
 
 post_save.connect(create_user_profile, sender=User)
 post_save.connect(save_user_profile, sender=User)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="image", null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+    # class Meta:
+        # verbose_name_plural = "Category"
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+            
+            super(Category, self).save(*args, **kwargs)
+    def count_post(self):
+        return Post.objects.filter(category=self).count()
+    
+class Post(models.Model):
+    
+    STATUS = {
+        ("Active", "Active"),
+        ("Draft", "Draft"),
+        ("Disabled", "Disabled")
+    }
+    
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(choices=STATUS, max_length=100, default="Active")
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    image = models.FileField(upload_to="image", null=True, blank=True)
+    view = models.IntegerField(default=0)
+    like = models.ManyToManyField(User, blank=True, related_name="likes_user")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["date"]
+        verbose_name_plural = "Post"
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) + "-" + shortuuid.uuid()[:2]
+            
+            super(Post, self).save(*args, **kwargs)
+            
+            
