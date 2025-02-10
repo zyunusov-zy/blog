ba700c19193fe2b03e97ffa550dd99f5430433ef
Zafarbek Yunusov <zyunusov30@gmail.com>
2025-02-10 13:21:36 +0500

    post view done

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('user/register/', api_views.RegisterView.as_view()),
     path('user/profile/<user_id>/', api_views.ProfileView.as_view()),
     
-    # Post End Points
+    # Post EndPoints
     path('post/category/list/', api_views.CategoryListAPIView.as_view()),
     path('post/category/posts/<category_slug>/', api_views.PostCategoryListAPIView.as_view()),
-
+    path('post/lists/', api_views.PostListsAPIView.as_view()),
+    path('post/detail/<slug>/', api_views.PostDetailAPIView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -62,3 +62,22 @@ class PostCategoryListAPIView(generics.ListAPIView):
         category_slug = self.kwargs['category_slug']
         category = api_models.Category.objects.get(slug=category_slug)
         return api_models.Post.objects.filter(category=category, status="Active")
+    
+class PostListsAPIView(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+    
+    def get_queryset(self):
+        return api_models.Post.objects.filter(status="Active")
+    
+class PostDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+    
+    def get_object(self):
+        slug = self.kwargs['slug']
+        post = api_models.Post.objects.get(slug=slug, status="Active")
+        post.view += 1
+        post.save()
+        return post
+        
